9cc9cfe5aca037370e480b6d762cefdedd2d0168
Kento Nishikawa <kentonishikawa00@gmail.com>
2026-04-14 15:07:13 -0300

    Se añaden las configuraciones del servidor al cual se tienen que conectar los clientes

---
+++ client.py
@@ -0,0 +1,8 @@
+import socket
+import threading
+
+# Configuración del servidor al que se conectará el cliente
+HOST = "127.0.0.1"   # Dirección del servidor (localhost)
+PORT = 6869          # Puerto del servidor
+
+
