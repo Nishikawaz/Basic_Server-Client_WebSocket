6f9f90166433017db9dd81c08c2cfcfe9933e88d
Kento Nishikawa <kentonishikawa00@gmail.com>
2026-04-23 02:43:09 -0300

    Se añade la función client_management y se modifica el nombre de la variable global CLIENT a SOCKET para tener mayor claridad

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ IP = "127.0.0.1"
 PORT = 6869
 BUFFER = 1024
 
-CLIENTS = []
+SOCKETS = [] 
 
 # Función para configurar inicialmente el socket_servidor
 def setup_server(ip, port):
@@ -26,26 +26,40 @@ def accept_client(socket_server):
     try:
         (socket_client, client_address) = socket_server.accept()
         socket_client.setblocking(False)
-        CLIENTS.append(socket_client)
+        SOCKETS.append(socket_client)
         print(f"Conexión establecida con el cliente - Conexión desde: {client_address[0]}:{client_address[1]}")
     except Exception as e:
         print(f"Error al aceptar conexión: {e}")
 
-# Función para desconectar al socket_cliente
+# Función para desconectar al socket_cliente 
 def disconnect(socket_client):
-    if socket_client in CLIENTS:
-        CLIENTS.remove(socket_client)
+    if socket_client in SOCKETS:
+        SOCKETS.remove(socket_client)
     try:
         socket_client.close()
     except:
         pass
     print(f"[SERVER] Cliente desconectado")
 
-# Función de envío de mensaje tipo broadcast - Envío masivo excepto al emisor
+# Función de envío de mensaje tipo broadcast (a todos menos al emisor)
 def broadcast(message, socket_sender):
-    for client in CLIENTS:
+    for client in SOCKETS:
         if client != socket_sender:
             try:
                 client.send(message.encode("utf-8"))
             except:
                 disconnect(client)
+
+# Función de gestión de los socket_clients. Verifica el envío de mensajes o desconexión
+def client_management(socket_client):
+    try: 
+        message = socket_client.recv(BUFFER)
+    except OSError:
+        disconnect(socket_client)
+        return
+    
+    if not message:
+        disconnect(socket_client)
+        return
+    
+    broadcast(message, socket_client)
